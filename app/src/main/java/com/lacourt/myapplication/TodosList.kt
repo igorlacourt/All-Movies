4686221536e0458868f8bb39e971062de842e82f
globalsys.igor <globalsys.igor@meupag.com.br>
2019-10-11 17:51:54 -0300

    More TODOs added ;)

--- app/src/main/java/com/lacourt/myapplication/TodosList.kt
+++ app/src/main/java/com/lacourt/myapplication/TodosList.kt
@@ -3,8 +3,10 @@ package com.lacourt.myapplication
 //TODO 1. Check how to deal with disposable for RxJava
 //TODO 2. Check how to make retrofit call in a kotlin way using generics
 //TODO 3. Check if it should a lateinit modifier for variables like recyclerview in MyListFragment and other classes
-//TODO 4. Make a secoond configuration for the case when backdrop_path is null, but poster_path is not
+//TODO 4. Make a second configuration for the case when backdrop_path is null, but poster_path is not
 //TODO 5. Add Navigation
 //TODO 6. Change item layout's width maybe it's better to set a fixed height, wrap_content width, and use matrix instead of centerCrop scaleType
-//TODO 7. MyList is not being updated when coming back from searchActivity (use activity fro result or BroadcastReceiver)
-
+//TODO 7. Adjust icons and bottom navigation
+//TODO 8. Set style hierarchy correctly
+//TODO 9. Remove hard coded string and colors
+//TODO 7. Add Dagger
