1d7d8fe63d56ec64e23890c6565a86a13e117f4c
globalsys.igor <globalsys.igor@meupag.com.br>
2020-01-03 16:45:48 -0300

    Two banners switch added to MyListFragment by setAdsVisibility function.

--- app/src/main/java/com/movies/allmovies/ui/mylist/MyListFragment.kt
+++ app/src/main/java/com/movies/allmovies/ui/mylist/MyListFragment.kt
@@ -13,19 +13,23 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.movies.allmovies.MainActivity
 import com.movies.allmovies.R
+import com.movies.allmovies.domainmodel.DomainMovie
 import com.movies.allmovies.ui.GridAdapter
 import com.movies.allmovies.ui.OnItemClick
-import com.movies.allmovies.util.BannerAds
 import com.movies.allmovies.viewmodel.MyListViewModel
-import org.junit.Test
 
 class MyListFragment : Fragment(), OnItemClick {
     private val onItemClick = this as OnItemClick
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewModel: MyListViewModel
     private lateinit var adapter: GridAdapter
+    private var adViewBottomRv: AdView? = null
+    private var adViewBottomScreen: AdView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +38,12 @@ class MyListFragment : Fragment(), OnItemClick {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_mylist, container, false)
         val progressBar: ProgressBar = root.findViewById(R.id.my_list_progress_circular)
+
+        Log.d("myadlog", "view = $view")
+        Log.d("myadlog", "view = ${view.toString()}")
+
+        adViewBottomRv = root.findViewById(R.id.mylist_adview_bottom_rv)
+        adViewBottomScreen = root.findViewById(R.id.mylist_adview_bottom_screen)
 
         val emptyList = root.findViewById<TextView>(R.id.edt_my_list_empty)
         emptyList.visibility = View.VISIBLE
@@ -53,14 +63,42 @@ class MyListFragment : Fragment(), OnItemClick {
                 emptyList.visibility = View.VISIBLE
             else{
                 emptyList.visibility = View.INVISIBLE
+                setAdsVisibility(list)
             }
 
             progressBar.visibility = View.INVISIBLE
         })
 
-        BannerAds.loadAds(context, root)
+        loadAdsBottomRv()
+        loadAdsBottomScreen()
 
         return root
+    }
+
+    private fun setAdsVisibility(list: List<DomainMovie>) {
+        if (list.size >= 7){
+            adViewBottomRv?.visibility = View.VISIBLE
+            adViewBottomScreen?.visibility = View.INVISIBLE
+        } else {
+            adViewBottomRv?.visibility = View.INVISIBLE
+            adViewBottomScreen?.visibility = View.VISIBLE
+        }
+    }
+
+    fun loadAdsBottomRv() {
+        if (context != null) {
+            MobileAds.initialize(context) {}
+            val adRequest = AdRequest.Builder().build()
+            adViewBottomRv?.loadAd(adRequest)
+        }
+    }
+
+    fun loadAdsBottomScreen() {
+        if (context != null) {
+            MobileAds.initialize(context) {}
+            val adRequest = AdRequest.Builder().build()
+            adViewBottomScreen?.loadAd(adRequest)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -86,22 +124,22 @@ class MyListFragment : Fragment(), OnItemClick {
         findNavController().navigate(myListToDetailsFragment)
     }
 
-    @Test
-    fun testNavigationToInGameScreen() {
-        // Create a mock NavController
-//        val mockNavController = mock(NavController::class.java)
-//
-//        // Create a graphical FragmentScenario for the TitleScreen
-//        val titleScenario = launchFragmentInContainer<MyListFragment>()
-//
-//        // Set the NavController property on the fragment
-//        titleScenario.onFragment { fragment ->
-//            Navigation.setViewNavController(fragment.requireView(), mockNavController)
-//        }
-//
-//        // Verify that performing a click prompts the correct Navigation action
-//        onView(ViewMatchers.withId(R.id.play_btn)).perform(ViewActions.click())
-//        verify(mockNavController).navigate(R.id.action_title_screen_to_in_game)
-    }
+//    @Test
+//    fun testNavigationToInGameScreen() {
+//        // Create a mock NavController
+////        val mockNavController = mock(NavController::class.java)
+////
+////        // Create a graphical FragmentScenario for the TitleScreen
+////        val titleScenario = launchFragmentInContainer<MyListFragment>()
+////
+////        // Set the NavController property on the fragment
+////        titleScenario.onFragment { fragment ->
+////            Navigation.setViewNavController(fragment.requireView(), mockNavController)
+////        }
+////
+////        // Verify that performing a click prompts the correct Navigation action
+////        onView(ViewMatchers.withId(R.id.play_btn)).perform(ViewActions.click())
+////        verify(mockNavController).navigate(R.id.action_title_screen_to_in_game)
+//    }
 
 }
