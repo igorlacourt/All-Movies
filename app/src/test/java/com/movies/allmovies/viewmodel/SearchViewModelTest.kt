406fe0bdd5e990476f05e83c5896e8b82711124c
igor lacourt <igorlacourt@gmail.com>
2020-07-24 16:06:32 -0300

    creates instant executor extension

---
+++ app/src/test/java/com/movies/allmovies/viewmodel/SearchViewModelTest.kt
@@ -0,0 +1,18 @@
+package com.movies.allmovies.viewmodel
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+@ExperimentalCoroutinesApi
+class SearchViewModelTest {
+    @BeforeEach
+    fun setupBeforeEach() {
+
+    }
+
+    @Test
+    fun `make reques, retorno 200`(){
+
+    }
+}
