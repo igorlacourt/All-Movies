8fbcb57b0d02b660c7a648656d9ce36e292d2b28
igor lacourt <igorlacourt@gmail.com>
2020-07-26 17:46:12 -0300

    implements unit test for network error case of the request for searching movie and make mutable livedata private in search viewmodel

--- app/src/main/java/com/movies/allmovies/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/movies/allmovies/viewmodel/SearchViewModel.kt
@@ -1,20 +1,33 @@
 package com.movies.allmovies.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.movies.allmovies.AppConstants
 import com.movies.allmovies.di.MainDispatcher
 import com.movies.allmovies.dto.MovieDTO
+import com.movies.allmovies.network.Error
 import com.movies.allmovies.network.NetworkResponse
 import com.movies.allmovies.network.TmdbApi
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
+import java.io.IOException
 import javax.inject.Inject
 
 class SearchViewModel @Inject constructor(private val tmdbApi: TmdbApi, @MainDispatcher val mainDispatcher: CoroutineDispatcher) : ViewModel() {
-    var searchResult: MutableLiveData<List<MovieDTO>> = MutableLiveData()
+    private var _searchResult = MutableLiveData<List<MovieDTO>>()
+    var searchResult: LiveData<List<MovieDTO>> = _searchResult
+
+    private var _apiErrorResult = MutableLiveData<Error>()
+    var apiErrorResult: LiveData<Error> = _apiErrorResult
+
+    private var _networkErrorResult = MutableLiveData<IOException>()
+    var networkErrorResult: LiveData<IOException> = _networkErrorResult
+
+    private var _unknownErrorResult = MutableLiveData<Throwable>()
+    var unknownErrorResult: LiveData<Throwable> = _unknownErrorResult
 
     val TAG = "calltest"
     fun searchMovie(title: String){
@@ -23,11 +36,11 @@ class SearchViewModel @Inject constructor(private val tmdbApi: TmdbApi, @MainDis
               when (response) {
                   is NetworkResponse.Success -> {
                       Log.d(TAG, "Success ${response.body.results[0].title}")
-                      searchResult.value = response.body.results
+                      _searchResult.value = response.body.results
                   }
-                  is NetworkResponse.ApiError -> Log.d(TAG, "ApiError ${response.body.message}")
-                  is NetworkResponse.NetworkError -> Log.d(TAG, "NetworkError")
-                  is NetworkResponse.UnknownError -> Log.d(TAG, "UnknownError")
+                  is NetworkResponse.ApiError -> { _apiErrorResult.value = response.body }
+                  is NetworkResponse.NetworkError -> { _networkErrorResult.value = response.error }
+                  is NetworkResponse.UnknownError -> { _unknownErrorResult.value = response.error }
               }
           }
     }
--- app/src/test/java/com/movies/allmovies/viewmodel/SearchViewModelTest.kt
+++ app/src/test/java/com/movies/allmovies/viewmodel/SearchViewModelTest.kt
@@ -11,11 +11,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
+import org.json.JSONObject
 import org.junit.Assert
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import retrofit2.Response
+import java.io.IOException
+import com.movies.allmovies.network.Error
 
 @ExtendWith(InstantExecutorExtension::class)
 class SearchViewModelTest {
@@ -45,6 +49,10 @@ class SearchViewModelTest {
         ))
     )
 
+    private val networkErrorResponse = NetworkResponse.NetworkError(IOException())
+    private val apiErrorResponse = NetworkResponse.ApiError(Error(500, "unavailable"), 500)
+    private val unknownErrorResponse = NetworkResponse.UnknownError(Throwable())
+
     @BeforeEach
     fun init() {
         Dispatchers.setMain(dispatcher)
@@ -68,5 +76,21 @@ class SearchViewModelTest {
 
         //Assert
         Assert.assertEquals(successResponse.body.results, viewModel?.searchResult?.value)
+    }
+
+    @Test
+    fun `make request, returns 400`() {
+        //Arrange
+        coEvery { tmdbApi.searchMovieSuspend(AppConstants.LANGUAGE, movieTitle, false) } answers {
+            networkErrorResponse
+        }
+        //Act
+        viewModel?.searchMovie(movieTitle)
+
+        //Assert
+        Assert.assertEquals(null, viewModel?.searchResult?.value)
+        Assert.assertEquals(null, viewModel?.apiErrorResult?.value)
+        Assert.assertEquals(null, viewModel?.unknownErrorResult?.value)
+        Assert.assertEquals(networkErrorResponse.error, viewModel?.networkErrorResult?.value)
     }
 }
