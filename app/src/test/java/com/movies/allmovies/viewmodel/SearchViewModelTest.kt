d19fa49d57bc552bae2afb9035d83962455c12aa
igor lacourt <igorlacourt@gmail.com>
2020-08-06 12:01:39 -0300

    fixes search viewmodel's unit tests

--- app/src/test/java/com/movies/allmovies/viewmodel/SearchViewModelTest.kt
+++ app/src/test/java/com/movies/allmovies/viewmodel/SearchViewModelTest.kt
@@ -1,113 +1,124 @@
 package com.movies.allmovies.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.movies.allmovies.AppConstants
 import com.movies.allmovies.dto.MovieDTO
 import com.movies.allmovies.dto.MovieResponseDTO
 import com.movies.allmovies.network.Error
 import com.movies.allmovies.network.NetworkResponse
 import com.movies.allmovies.network.TmdbApi
-import io.mockk.coEvery
-import io.mockk.mockk
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
-import org.junit.Assert
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 import java.io.IOException
 
-
-@ExtendWith(InstantExecutorExtension::class)
+@RunWith(MockitoJUnitRunner::class)
 class SearchViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var tmdbApi: TmdbApi
+
     private val dispatcher = TestCoroutineDispatcher()
-    private val tmdbApi = mockk<TmdbApi>()
     private var viewModel: SearchViewModel? = null
     private val movieTitle = "nemo"
     private val successResponse = NetworkResponse.Success(
-        MovieResponseDTO(listOf(
-            MovieDTO(
-                false,
-                "backdrop-path",
-                arrayListOf("1", "2", "3", "4"),
-                arrayListOf("comedy, drama, adventure"),
-                1234,
-                "en-US",
-                "Finding Nemo",
-                "movie overview",
-                90.0,
-                "poster-path",
-                "2020",
-                "Finding Nemo",
-                false,
-                10.0,
-                200000
+        MovieResponseDTO(
+            listOf(
+                MovieDTO(
+                    false,
+                    "backdrop-path",
+                    arrayListOf("1", "2", "3", "4"),
+                    arrayListOf("comedy, drama, adventure"),
+                    1234,
+                    "en-US",
+                    "Finding Nemo",
+                    "movie overview",
+                    90.0,
+                    "poster-path",
+                    "2020",
+                    "Finding Nemo",
+                    false,
+                    10.0,
+                    200000
+                )
             )
-        ))
+        )
     )
 
     private val networkErrorResponse = NetworkResponse.NetworkError(IOException())
     private val apiErrorResponse = NetworkResponse.ApiError(Error(500, "unavailable"), 500)
     private val unknownErrorResponse = NetworkResponse.UnknownError(Throwable())
 
-    @BeforeEach
+    @Before
     fun init() {
         viewModel = SearchViewModel(tmdbApi, dispatcher)
         Dispatchers.setMain(dispatcher)
     }
 
-    @AfterEach
+    @After
     fun tearDown() {
-        viewModel = null
         Dispatchers.resetMain()
         dispatcher.cleanupTestCoroutines()
     }
 
     @Test
-    fun `make request, returns 200`() {
+    fun `make request, returns 200`() = dispatcher.runBlockingTest {
         //Arrange
-        coEvery { tmdbApi.searchMovieSuspend(AppConstants.LANGUAGE, movieTitle, false) } answers {
+        `when`(tmdbApi.searchMovieSuspend(AppConstants.LANGUAGE, movieTitle, false)).thenReturn(
             successResponse
-        }
+        )
         //Act
         viewModel?.searchMovie(movieTitle)
 
         //Assert
-        Assert.assertEquals(null, viewModel?.errorMessage?.value)
-        Assert.assertEquals(false, viewModel?.errorScreenVisibility?.value)
-        Assert.assertEquals(successResponse.body.results, viewModel?.searchResult?.value)
+        assertEquals(null, viewModel?.errorMessage?.value)
+        assertEquals(false, viewModel?.errorScreenVisibility?.value)
+        assertEquals(successResponse.body.results, viewModel?.searchResult?.value)
     }
 
     @Test
-    fun `make request, returns 400`() {
+    fun `make request, returns 400`() = dispatcher.runBlockingTest {
         //Arrange
-        coEvery { tmdbApi.searchMovieSuspend(AppConstants.LANGUAGE, movieTitle, false) } answers {
+        `when`(tmdbApi.searchMovieSuspend(AppConstants.LANGUAGE, movieTitle, false)).thenReturn(
             networkErrorResponse
-        }
+        )
         //Act
         viewModel?.searchMovie(movieTitle)
 
         //Assert
-        Assert.assertEquals(null, viewModel?.searchResult?.value)
-        Assert.assertEquals(true, viewModel?.errorScreenVisibility?.value)
-        Assert.assertEquals(AppConstants.NETWORK_ERROR_MESSAGE, viewModel?.errorMessage?.value)
+        assertEquals(null, viewModel?.searchResult?.value)
+        assertEquals(true, viewModel?.errorScreenVisibility?.value)
+        assertEquals(AppConstants.NETWORK_ERROR_MESSAGE, viewModel?.errorMessage?.value)
     }
+
 
     @Test
-    fun `make request, returns 500`() {
-        //Arrange
-        coEvery { tmdbApi.searchMovieSuspend(AppConstants.LANGUAGE, movieTitle, false) } answers {
+    fun `make request, returns 500`() = dispatcher.runBlockingTest {
+        // Arrange
+        `when`(tmdbApi.searchMovieSuspend(AppConstants.LANGUAGE, movieTitle, false)).thenReturn(
             apiErrorResponse
-        }
+        )
         //Act
         viewModel?.searchMovie(movieTitle)
 
-
         //Assert
-        Assert.assertEquals(null, viewModel?.searchResult?.value)
-        Assert.assertEquals(true, viewModel?.errorScreenVisibility?.value)
-        Assert.assertEquals(AppConstants.API_ERROR_MESSAGE, viewModel?.errorMessage?.value)
+        assertEquals(null, viewModel?.searchResult?.value)
+        assertEquals(true, viewModel?.errorScreenVisibility?.value)
+        assertEquals(AppConstants.API_ERROR_MESSAGE, viewModel?.errorMessage?.value)
     }
+
 }
