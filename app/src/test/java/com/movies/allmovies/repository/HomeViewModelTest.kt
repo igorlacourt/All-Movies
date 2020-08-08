cd8ef80cc1dcaf2d30656322b6e5038b0a7d85b8
igor lacourt <igorlacourt@gmail.com>
2020-08-07 22:52:18 -0300

    completes unit tests for getting lists of movies in the home viewmodel

--- app/src/test/java/com/movies/allmovies/repository/HomeViewModelTest.kt
+++ app/src/test/java/com/movies/allmovies/repository/HomeViewModelTest.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.movies.allmovies.AppConstants
 import com.movies.allmovies.domainmappers.toDomainMovieList
+import com.movies.allmovies.domainmodel.DomainMovie
+import com.movies.allmovies.domainmodel.MyListItem
 import com.movies.allmovies.dto.MovieDTO
 import com.movies.allmovies.dto.MovieResponseDTO
 import com.movies.allmovies.network.NetworkResponse
 import com.movies.allmovies.network.TmdbApi
+import com.movies.allmovies.viewmodel.HomeResult
 import com.movies.allmovies.viewmodel.HomeViewModel
-import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
@@ -18,9 +21,7 @@ import kotlinx.coroutines.test.setMain
 import org.junit.*
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
-
 
 @RunWith(MockitoJUnitRunner::class)
 class HomeViewModelTest {
@@ -35,7 +36,7 @@ class HomeViewModelTest {
 
     private val dispatcher = TestCoroutineDispatcher()
 
-    private var homeDataSource: HomeDataSourceImpl? = null
+    private var homeDataSourceMock: HomeDataSourceMock? = null
     private var viewModel: HomeViewModel? = null
     private val movieDTOMock = MovieDTO(false, "", arrayListOf("0","0","0"), arrayListOf("","",""), 0, "", "", "", 0.0, "", "", "", false, 0.0, 0)
     private val successResponseMock = NetworkResponse.Success(MovieResponseDTO(listOf(movieDTOMock)))
@@ -48,40 +49,91 @@ class HomeViewModelTest {
 
     @Before
     fun init() {
-        homeDataSource = HomeDataSourceImpl(context, tmdbApi)
-        viewModel = HomeViewModel(context, tmdbApi, homeDataSource!!, dispatcher)
         Dispatchers.setMain(dispatcher)
     }
 
     @After
     fun tearDown() {
         viewModel = null
-        homeDataSource = null
+        homeDataSourceMock = null
         Dispatchers.resetMain()
         dispatcher.cleanupTestCoroutines()
     }
 
     // title should have: given, when, then
     @Test
-    fun `make all 4 requests and all of them is successful`() = dispatcher.runBlockingTest {
+    fun `get lists of movies from the data source, and all lists come with no error`() = dispatcher.runBlockingTest {
         // Arrange
-        `when`(tmdbApi.getTrendingMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
-            successResponseMock
-        )
-        `when`(tmdbApi.getUpcomingMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
-            successResponseMock
-        )
-        `when`(tmdbApi.getPopularMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
-            successResponseMock
-        )
-        `when`(tmdbApi.getTopRatedMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
-            successResponseMock
-        )
+        homeDataSourceMock = HomeDataSourceMock(resultListsMock, success = true)
+        viewModel = HomeViewModel(context, tmdbApi, homeDataSourceMock!!, dispatcher)
         // Act
         viewModel?.getListOfMovies()
         // Assert
         Assert.assertEquals(resultListsMock, viewModel?.listsOfMovies?.value)
-        assertEquals(false, viewModel?.errorScreenVisibility?.value)
-        assertEquals(null, viewModel?.errorMessage?.value)
+        Assert.assertEquals(false, viewModel?.errorScreenVisibility?.value)
+        Assert.assertEquals(null, viewModel?.errorMessage?.value)
     }
+
+    @Test
+    fun `get lists of movies from the data source, but an error occurs in one or more of the requests`() = dispatcher.runBlockingTest {
+        // Arrange
+        homeDataSourceMock = HomeDataSourceMock(success = false)
+        viewModel = HomeViewModel(context, tmdbApi, homeDataSourceMock!!, dispatcher)
+        // Act
+        viewModel?.getListOfMovies()
+        // Assert
+        Assert.assertEquals(null, viewModel?.listsOfMovies?.value)
+        Assert.assertEquals(true, viewModel?.errorScreenVisibility?.value)
+        Assert.assertEquals(AppConstants.API_ERROR_MESSAGE, viewModel?.errorMessage?.value)
+    }
+
+//    @Test
+//    fun `make all 4 requests and all of them is successful`() = dispatcher.runBlockingTest {
+//        // Arrange
+//        `when`(tmdbApi.getTrendingMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
+//            successResponseMock
+//        )
+//        `when`(tmdbApi.getUpcomingMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
+//            successResponseMock
+//        )
+//        `when`(tmdbApi.getPopularMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
+//            successResponseMock
+//        )
+//        `when`(tmdbApi.getTopRatedMoviesSuspend(AppConstants.LANGUAGE, 1)).thenReturn(
+//            successResponseMock
+//        )
+//        // Act
+//        viewModel?.getListOfMovies()
+//        // Assert
+//        Assert.assertEquals(resultListsMock, viewModel?.listsOfMovies?.value)
+//        assertEquals(false, viewModel?.errorScreenVisibility?.value)
+//        assertEquals(null, viewModel?.errorMessage?.value)
+//    }
+}
+
+class HomeDataSourceMock(private val resultLists: ArrayList<Collection<DomainMovie>>? = null, private val success: Boolean) : HomeDataSource {
+    override suspend fun getListsOfMovies(dispatcher: CoroutineDispatcher, homeResultCallback: (result: HomeResult) -> Unit) {
+        if(success){
+            resultLists?.let { homeResultCallback(HomeResult.Success(resultLists)) }
+        } else {
+            homeResultCallback(HomeResult.ApiError(400))
+        }
+    }
+
+    override fun isTopMovieInDatabase(id: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun refresh() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun insert(myListItem: MyListItem) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(id: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
