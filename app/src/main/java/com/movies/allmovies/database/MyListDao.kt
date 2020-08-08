8c090dd1277d805b36f8715569c83ce73c4c684e
igor lacourt <igorlacourt@gmail.com>
2020-08-08 17:27:49 -0300

    removes unused database queries

--- app/src/main/java/com/movies/allmovies/database/MyListDao.kt
+++ app/src/main/java/com/movies/allmovies/database/MyListDao.kt
@@ -1,20 +1,15 @@
 package com.movies.allmovies.database
 
-import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.movies.allmovies.domainmodel.MyListItem
-import com.movies.allmovies.dto.DbMovieDTO
 
 @Dao
 interface MyListDao {
     @Query("SELECT EXISTS (SELECT id FROM mylist WHERE id = :id)")
     fun exists(id: Int?): Boolean
-//
-//    @Query("SELECT id FROM mylist WHERE id = :id")
-//    fun getById(id: Int): Single<Int>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(item: MyListItem)
@@ -24,10 +19,4 @@ interface MyListDao {
 
     @Query("DELETE FROM mylist WHERE id = :id")
     fun deleteById(id: Int)
-
-    @Query("SELECT * FROM mylist ORDER BY release_date DESC")
-    fun dateDesc(): DataSource.Factory<Int, DbMovieDTO>
-
-    @Query("SELECT * FROM mylist ORDER BY release_date ASC")
-    fun dateAsc(): DataSource.Factory<Int, DbMovieDTO>
 }
