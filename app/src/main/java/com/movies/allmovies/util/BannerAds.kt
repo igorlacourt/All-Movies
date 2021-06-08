311390e887b604f836392e20b3fa451fc554b66e
igor lacourt <igorlacourt@gmail.com>
2021-06-08 12:43:27 -0300

    fix adview memory leak cherry pick 1

--- app/src/main/java/com/movies/allmovies/util/BannerAds.kt
+++ app/src/main/java/com/movies/allmovies/util/BannerAds.kt
@@ -2,18 +2,30 @@ package com.movies.allmovies.util
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.movies.allmovies.R
 
-object BannerAds {
-    fun loadAds(context: Context?, view: View) {
+class BannerAds(view: View) {
+    private var mAdView = view.findViewById<AdView>(R.id.adView)
+    private var adRequest = AdRequest.Builder().build()
+
+    fun loadAds(context: Context?) {
         if (context != null) {
-            var mAdView = view.findViewById<AdView>(R.id.adView)
             MobileAds.initialize(context) {}
-            val adRequest = AdRequest.Builder().build()
             mAdView.loadAd(adRequest)
+        }
+    }
+    fun removeAds() {
+        if (mAdView != null) {
+            (mAdView.parent as ViewGroup).removeView(mAdView)
+            mAdView.adListener = null
+            mAdView.removeAllViews()
+            mAdView.destroy()
+            mAdView = null
+            adRequest = null
         }
     }
 }
